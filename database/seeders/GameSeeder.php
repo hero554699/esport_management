ef1c486eca55dbfe29aa6a030f7a3af199762aec
Hero <h.obillo.554699@umindanao.edu.ph>
2026-05-25 13:52:46 +0800

    latest-save

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -9,12 +9,16 @@ class GameSeeder extends Seeder
 {
     public function run(): void
     {
-        
+        $games = [
+            ['name' => 'Counter Strike 2', 'slug' => 'cs2', 'platform' => 'pc', 'is_active' => true],
+            ['name' => 'VALORANT', 'slug' => 'valorant', 'platform' => 'pc', 'is_active' => true],
+            ['name' => 'League of Legends', 'slug' => 'lol', 'platform' => 'pc', 'is_active' => true],
+            ['name' => 'Mobile Legends', 'slug' => 'ml', 'platform' => 'mobile', 'is_active' => true],
+        ];
 
         foreach ($games as $game) {
-            // Use updateOrCreate instead of firstOrCreate
             Game::updateOrCreate(
-                ['slug' => $game['slug']], // Use slug as unique identifier
+                ['slug' => $game['slug']],
                 $game
             );
         }
